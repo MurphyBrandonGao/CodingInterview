1906e9f1a5c434530e223b544a33348c5e01cce8
GaoBo <18279552824@163.com>
2019-03-16 16:31:17 +0800

    modify problem 14 class solution1

--- 14_CuttingRope/src/Solution1.java
+++ 14_CuttingRope/src/Solution1.java
@@ -4,6 +4,8 @@ import jdk.nashorn.internal.ir.IfNode;
  * @author Dell
  * @create 2019-03-15 17:55
  */
+
+//贪婪法 时间复杂度O(n),空间复杂度O(1)
 public class Solution1 {
     public int integerBreak(int n) {
         if (n < 2)
@@ -18,10 +20,10 @@ public class Solution1 {
 
         //当绳子最后剩下的长度为4时，不能再减去长度为3的绳子段
         //此时更好的方法是把绳子剪成长度为2的两段，因为2*2>3*1
-        if (n - timesOf3 * 3 == 1)
+        if (n - timesOf3 * 3 == 1) // n % timesOf3 == 1
             timesOf3 -= 1;
 
-        int timesOf2 = (n - timesOf3 * 3) / 2;
+        int timesOf2 = (n - timesOf3 * 3) >> 1;
         return (int) ((Math.pow(3, timesOf3)) * (Math.pow(2, timesOf2)));
     }
 }
