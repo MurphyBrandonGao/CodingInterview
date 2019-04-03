14edc964e0a5db1d96a7ba0cd59c4f197eb4b089
GaoBo <18279552824@163.com>
2019-04-03 17:12:05 +0800

    add problem 30

---
+++ 30_MinInStack/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+ * @author Dell
+ * @create 2019-04-02 10:12
+ */
+
+// 面试题30：包含min函数的栈
+// 题目：定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的min
+// 函数。在该栈中，调用min、push及pop的时间复杂度都是O(1)。
+public class Solution {
+
+    private Stack<Integer> dataStack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public void push(int node) {
+        dataStack.push(node);
+        minStack.push(minStack.isEmpty() ? node : Math.min(minStack.peek(), node));
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
