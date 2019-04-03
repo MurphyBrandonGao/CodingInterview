c7c9c1922c44c5169fe600942ecfa4dadb3db2e9
GaoBo <18279552824@163.com>
2019-04-03 17:15:01 +0800

    add problem 32_03

---
+++ 32_03_PrintTreesInZigzag/src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Dell
+ * @create 2019-04-03 16:42
+ */
+// 面试题32（三）：之字形打印二叉树
+// 题目：请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺
+// 序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，
+// 其他行以此类推。
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        boolean reverse = false;
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int cnt = queue.size();
+            while (cnt-- > 0) {
+                TreeNode node = queue.poll();
+                if (node == null)
+                    continue;
+                list.add(node.val);
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+            if (reverse)
+                Collections.reverse(list);
+            reverse = !reverse;
+            if (list.size() != 0)
+                ret.add(list);
+        }
+        return ret;
+    }
+}
---
+++ 32_03_PrintTreesInZigzag/src/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * @author Dell
+ * @create 2019-04-03 13:40
+ */
+// 面试题32（三）：之字形打印二叉树
+// 题目：请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺
+// 序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，
+// 其他行以此类推。
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
