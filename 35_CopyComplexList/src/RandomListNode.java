0f81ecc31d73d75bc6b732e826124d7ddac873cb
GaoBo <18279552824@163.com>
2019-04-08 20:58:14 +0800

    add problem 35

---
+++ 35_CopyComplexList/src/RandomListNode.java
@@ -0,0 +1,13 @@
+/**
+ * @author Dell
+ * @create 2019-04-08 20:09
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
---
+++ 35_CopyComplexList/src/Solution.java
@@ -0,0 +1,61 @@
+/**
+ * @author Dell
+ * @create 2019-04-08 20:09
+ */
+// 面试题35：复杂链表的复制
+// 题目：请实现函数ComplexListNode* Clone(ComplexListNode* pHead)，复
+// 制一个复杂链表。在复杂链表中，每个结点除了有一个m_pNext指针指向下一个
+// 结点外，还有一个m_pSibling 指向链表中的任意结点或者nullptr。
+
+/*
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
+*/
+public class Solution {
+    /**
+     * 复杂链表的复制
+     * @param pHead 链表头结点
+     * @return 复制的链表头结点
+     */
+    public RandomListNode clone(RandomListNode pHead) {
+        if (pHead == null)
+            return null;
+
+        //插入新节点
+        RandomListNode cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = new RandomListNode(cur.label);
+            clone.next = cur.next;
+            cur.next = clone;
+            cur = clone.next;
+        }
+
+        //建立random链接
+        cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = cur.next;
+            if (cur.random != null)
+                clone.random = cur.random.next;
+
+            cur = clone.next;
+        }
+
+        //拆分
+        cur = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        while (cur.next != null) {
+            RandomListNode next = cur.next;
+            cur.next = next.next;
+            cur = next;
+        }
+
+        return pCloneHead;
+    }
+}
