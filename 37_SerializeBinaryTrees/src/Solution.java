1e20f0309560f6476906b60fd186feaf0028ae45
GaoBo <18279552824@163.com>
2019-04-10 17:24:03 +0800

    37 solved

---
+++ 37_SerializeBinaryTrees/src/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * @author Dell
+ * @create 2019-04-10 14:30
+ */
+
+// 面试题37：序列化二叉树
+// 题目：请实现两个函数，分别用来序列化和反序列化二叉树。
+
+public class Solution {
+
+    private String deserializeStr;
+
+    String Serialize(TreeNode root) {
+        if (root == null)
+            return "#";
+        return root.val + " " + Serialize(root.left) + " " + Serialize(root.right);
+    }
+
+    TreeNode Deserialize(String str) {
+        deserializeStr = str;
+        return Deserialize();
+    }
+
+    private TreeNode Deserialize() {
+        if (deserializeStr.length() == 0)
+            return null;
+        int index = deserializeStr.indexOf(" ");
+        String node = index == -1 ? deserializeStr : deserializeStr.substring(0, index);
+        deserializeStr = index == -1 ? "" : deserializeStr.substring(index + 1);
+        if (node.equals("#"))
+            return null;
+        int val = Integer.valueOf(node);
+        TreeNode t = new TreeNode(val);
+        t.left = Deserialize();
+        t.right = Deserialize();
+        return t;
+    }
+}
