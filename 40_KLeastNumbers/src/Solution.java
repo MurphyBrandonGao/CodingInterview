236a774749f868acd1d1c375ad0fe738bfae9723
GaoBo <18279552824@163.com>
2019-04-15 15:53:10 +0800

    40 solution modified

--- 40_KLeastNumbers/src/Solution.java
+++ 40_KLeastNumbers/src/Solution.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 // 面试题40：最小的k个数
 // 题目：输入n个整数，找出其中最小的k个数。例如输入4、5、1、6、2、7、3、8
 // 这8个数字，则最小的4个数字是1、2、3、4。
+
+// 时间复杂度为O(n),空间复杂度为O(1)
+// 需要修改输入的数组
 public class Solution {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         ArrayList<Integer> ret = new ArrayList<>();
