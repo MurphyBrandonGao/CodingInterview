a3b4bd8635d386763cc06c88f6eb63ae3c227696
GaoBo <18279552824@163.com>
2019-05-03 17:31:33 +0800

    55_01 solved

---
+++ 55_01_TreeDepth/src/Solution.java
@@ -0,0 +1,13 @@
+/**
+ * @author Dell
+ * @create 2019-05-02 11:19
+ */
+// 面试题55（一）：二叉树的深度
+// 题目：输入一棵二叉树的根结点，求该树的深度。从根结点到叶结点依次经过的
+// 结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+
+public class Solution {
+    public int TreeDepth(TreeNode root) {
+        return root == null ? 0 : 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
+    }
+}
---
+++ 55_01_TreeDepth/src/TreeNode.java
@@ -0,0 +1,15 @@
+/**
+ * @author Dell
+ * @create 2019-05-02 10:59
+ */
+public class TreeNode {
+
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
