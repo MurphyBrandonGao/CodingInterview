f752e27b998c6215daa5dec8d20c46e211b7f2d6
GaoBo <18279552824@163.com>
2019-05-18 23:47:14 +0800

    64 solved

---
+++ 64_Accumulate/src/Solution.java
@@ -0,0 +1,14 @@
+/**
+ * @author Dell
+ * @create 2019-05-11 16:35
+ */
+// 面试题64：求1+2+…+n
+// 题目：求1+2+…+n，要求不能使用乘除法、for、while、if、else、switch、case
+// 等关键字及条件判断语句（A?B:C）
+public class Solution {
+    public int Sum_Solution(int n) {
+        int sum = n;
+        boolean b = (n > 0) && ((sum += Sum_Solution(n - 1)) > 0);
+        return sum;
+    }
+}
